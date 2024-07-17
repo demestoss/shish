3c13b84b436dad5897ea426286e6b51105968354
Dmitriy Popov <dmitriiy.popov@cteleport.com>
2024-07-17 17:24:19 +0200

    4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
+use std::str::FromStr;
+
+fn main() -> anyhow::Result<()> {
+    loop {
+        print_line_start()?;
+        let command = get_user_command()?;
+        handle_user_command(&command);
+    }
+}
 
 fn print_line_start() -> Result<(), io::Error> {
     print!("$ ");
@@ -13,11 +23,21 @@ fn get_user_command() -> Result<String, io::Error> {
     Ok(input)
 }
 
-fn main() -> Result<(), io::Error> {
-    loop {
-        print_line_start()?;
-        let command = get_user_command()?;
+fn handle_user_command(command: &str) {
+    let command = command.trim();
+    let (command_name, command_params) = command.split_once(" ").unwrap_or((command, ""));
 
-        println!("{}: command not found", command.trim_end());
+    match command_name {
+        "" => {}
+        "exit" => {
+            let status_code = i8::from_str(command_params);
+            match status_code {
+                Ok(code) => exit(code as i32),
+                _ => exit(0),
+            }
+        }
+        _ => {
+            println!("{command_name}: command not found");
+        }
     }
 }
