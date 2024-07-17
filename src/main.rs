4a46c7db5f845d3621ba48cf17fc0cb6b6c81edd
Dmitriy Popov <dmitriiy.popov@cteleport.com>
2024-07-17 22:28:05 +0200

    EI0 stage

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ fn handle_user_input(input: &str) -> Result<(), io::Error> {
         "echo" => println!("{command_args}"),
         "exit" => handle_exit_command(command_args),
         "type" => handle_type_command(command_args),
+        "pwd" => handle_pwd_command(),
         _ => match find_command_path(command) {
             Some(path) => {
                 let output = Command::new(path)
@@ -58,7 +59,7 @@ fn handle_exit_command(args: &str) {
 fn handle_type_command(args: &str) {
     args.split_whitespace().for_each(|param| match param {
         "" => {}
-        "echo" | "exit" | "type" => println!("{param} is a shell builtin"),
+        "echo" | "exit" | "type" | "pwd" => println!("{param} is a shell builtin"),
         command => match find_command_path(command) {
             Some(path) => println!("{command} is {path}"),
             None => println!("{param}: not found"),
@@ -72,4 +73,11 @@ fn find_command_path(command: &str) -> Option<String> {
         let path = format!("{dir}/{command}");
         Path::new(&path).exists().then_some(path)
     })
+}
+
+fn handle_pwd_command() {
+    match env::current_dir() {
+        Ok(path) => println!("{}", path.display()),
+        Err(err) => eprintln!("{err}"),
+    }
 }
