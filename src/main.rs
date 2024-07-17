53a7ee55e6e7463db3bc24cf196356dccdbd8c44
Dmitriy Popov <dmitriiy.popov@cteleport.com>
2024-07-17 17:43:10 +0200

    5th stage

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use std::str::FromStr;
 fn main() -> anyhow::Result<()> {
     loop {
         print_line_start()?;
-        let command = get_user_command()?;
-        handle_user_command(&command);
+        let command = get_user_input()?;
+        handle_user_input(&command);
     }
 }
 
@@ -16,31 +16,24 @@ fn print_line_start() -> Result<(), io::Error> {
     io::stdout().flush()
 }
 
-fn get_user_command() -> Result<String, io::Error> {
+fn get_user_input() -> Result<String, io::Error> {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input)?;
     Ok(input)
 }
 
-fn handle_user_command(command: &str) {
-    let command = command.trim();
-    let (command_name, command_params) = command.split_once(" ").unwrap_or((command, ""));
+fn handle_user_input(input: &str) {
+    let input = input.trim();
+    let (command_name, command_params) = input.split_once(" ").unwrap_or((input, ""));
 
     match command_name {
-        "" => {}
-        "echo" => {
-            println!("{command_params}");
-        }
+        "" => (),
+        "echo" => println!("{command_params}"),
         "exit" => {
-            let status_code = i8::from_str(command_params);
-            match status_code {
-                Ok(code) => exit(code as i32),
-                _ => exit(0),
-            }
+            let status_code = i8::from_str(command_params).unwrap_or_default();
+            exit(status_code as i32);
         }
-        _ => {
-            println!("{command_name}: command not found");
-        }
-    }
+        _ => println!("{command_name}: command not found"),
+    };
 }
