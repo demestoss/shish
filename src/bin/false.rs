a354fa2fe292c23ee97866ba6de279963d8d2bcf
Demes Toss <me@demestoss.com>
2024-08-04 17:06:05 +0200

    tests start

--- src/bin/false.rs
+++ src/bin/false.rs
@@ -1,3 +1,11 @@
+use assert_cmd::Command;
+
 pub fn main() {
     std::process::abort();
+}
+
+#[test]
+fn false_failure() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
--- src/bin/true.rs
+++ src/bin/true.rs
@@ -1,4 +1,11 @@
+use assert_cmd::Command;
+
 pub fn main() {
-    println!("dsdsds");
     std::process::exit(0);
+}
+
+#[test]
+fn true_success() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
 }
